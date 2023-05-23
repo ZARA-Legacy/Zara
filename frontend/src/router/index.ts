1b1879f8dfed2f79bf3adc9cc770eb489e53a1a1
Wael Dghim <104037074+Wael-Dghim@users.noreply.github.com>
2023-05-23 18:36:39 +0100

    login view done

---
+++ frontend/src/router/index.ts
@@ -0,0 +1,9 @@
+import { createRouter, createWebHistory } from "vue-router";
+import LoginView from "../views/LoginView.vue";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [{ path: "/", name: "home", component: LoginView }],
+});
+
+export default router;
