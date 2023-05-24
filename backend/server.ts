d4b88c5831058a3d69e90699bccf7d2aa2cae0be
Wael Dghim <104037074+Wael-Dghim@users.noreply.github.com>
2023-05-25 00:25:08 +0100

    sends designed emails

--- backend/server.ts
+++ backend/server.ts
@@ -23,10 +23,58 @@ app.use("/products", routerP);
 app.use("/cart", cartRoute);
 app.use("/api", emailRoute);
 
-sequelize.sync().then(() => {
-  app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-  });
-});
+sequelize
+  .sync()
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .then(() => {
+    async function insertDummyData() {
+      try {
+        // Iterate over the data array
+        for (const product of data) {
+          const {
+            name,
+            image,
+            price,
+            quantity,
+            gender,
+            category,
+            description,
+          } = product;
 
-// Call the function to insert dummy data
+          // Check if a similar entry already exists
+          const existingProduct = await Product.findOne({
+            where: {
+              name,
+              gender,
+              category,
+            },
+          });
+
+          if (!existingProduct) {
+            // No similar entry found, proceed with insertion
+            await Product.create({
+              name,
+              image,
+              price,
+              quantity,
+              gender,
+              category,
+              description,
+            });
+            console.log("Product inserted successfully");
+          } else {
+            console.log("Similar entry already exists");
+          }
+        }
+      } catch (error) {
+        console.log(`Error: ${error}`);
+      }
+    }
+
+    // Call the function to insert dummy data
+    insertDummyData();
+  });
