ca30e3b8058c161b85f1fc87515066cd34a213f2
AdamSaafy <adamsaafy5@gmail.com>
2023-05-26 17:30:10 +0100

    crud admin dashboard backend done

--- backend/controllers/admin/adminController.ts
+++ backend/controllers/admin/adminController.ts
@@ -1,5 +1,11 @@
 import { Request, Response } from "express";
 import User from "../../models/User";
+import Product from "../../models/Product";
+import  Sequelize from "sequelize"
+
+
+
+// Users
 
 export const getAllUsers = async (req: Request, res: Response) => {
   try {
@@ -45,6 +51,99 @@ export const deleteUser = async (req: Request, res: Response) => {
   try {
     await User.destroy({ where: { id } });
     res.status(200).send("User deleted");
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
+
+// Products
+export const getAllProducts = async (req: Request, res: Response) => {
+  try {
+    const products = await Product.findAll();
+    res.status(200).json(products);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
+export const createProduct = async (req: Request, res: Response) => {
+  try {
+    const { name, image, price, quantity, gender, category, description } = req.body;
+    const product = await Product.create({
+      name,
+      image,
+      price,
+      quantity,
+      gender,
+      category,
+      description,
+    });
+    res.status(201).json(product);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
+export const getOneProduct = async (req: Request, res: Response) => {
+  const id = req.params.id;
+  try {
+    const product = await Product.findOne({ where: { id } });
+    res.status(200).json(product);
+  }
+  catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
+export const deleteProduct = async (req: Request, res: Response) => {
+  const id = req.params.id;
+
+  try {
+    await Product.destroy({ where: { id } });
+    res.status(200).send("Product deleted");
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
+export const updateProduct = async (req: Request, res: Response) => {
+  const id = req.params.id;
+  try {
+    const { name, image, price, quantity, gender, category, description } = req.body;
+    const product = await Product.findOne({ where: { id } });
+
+    if (!product) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
+
+    await product.update({
+      name,
+      image,
+      price,
+      quantity,
+      gender,
+      category,
+      description,
+    });
+
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
+
+export const searchProductByName = async (req, res) => {
+  const { name } = req.query;
+  try {
+    const products = await Product.findAll({ where: { name: { [Sequelize.Op.like]: `%${name}%` } } });
+
+    if (products.length === 0) {
+      return res.status(404).json({ error: 'No products found' });
+    }
+
+    res.status(200).json(products);
   } catch (error) {
     console.log(error);
     res.status(500).json(error);
--- backend/routes/admin/adminRoute.ts
+++ backend/routes/admin/adminRoute.ts
@@ -4,6 +4,12 @@ import {
   getOneUser,
   updateAdmin,
   deleteUser,
+  getAllProducts,
+  getOneProduct,
+  createProduct,
+  deleteProduct,
+  updateProduct,
+  searchProductByName,
 } from "../../controllers/admin/adminController";
 
 import { authorize } from "../../middleware/auth";
@@ -17,8 +23,18 @@ router.get("/users/one/:id", getOneUser);
 
 router.put("/users/:id", updateAdmin);
 
-router.delete("/users/:id", deleteUser);
 
+router.delete("/users/:id", deleteUser);
 //Products
+
+router.get("/products/all", getAllProducts);
+router.get("/products/one/:id",getOneProduct)
+router.get("/products/:name",searchProductByName)
+router.post("/products/add",createProduct)
+router.delete("/products/:id", deleteProduct);
+router.put("/products/:id", updateProduct);
+
+
+
 
 export default router;
