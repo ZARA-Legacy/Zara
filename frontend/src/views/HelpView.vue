62e3a320e325f97cf6bda7aa328d09871dfa240c
AdamSaafy <adamsaafy5@gmail.com>
2023-05-26 02:33:12 +0100

    some changes and some styling help

--- frontend/src/views/HelpView.vue
+++ frontend/src/views/HelpView.vue
@@ -19,9 +19,10 @@
   
       const fetchAnswer = async () => {
         try {
-          const response = await axios.get(`http://localhost:3000/help/${typedQuestion.value}`);
+          const response = await axios.get(`http://localhost:3000/help/one/${typedQuestion.value}`);
           answer.value = response.data.answer;
-          console.log(answer)
+          console.log(typedQuestion,'typedQuestion')
+          console.log(answer,'answer')
         } catch (error) {
           console.error(error);
         }
@@ -38,7 +39,7 @@
   
   
   <style scoped>
-  .container {
+  /* .container {
     background-image: url('https://static.zara.net/photos///contents/mkt/spots/ss23-help-customer/subhome-xmedia-10-north//w/1839/IMAGE-landscape-web-27470945-8c31-433a-9eaf-392114b6a91d-default_0.jpg?ts=1682518360676%27');
     background-repeat: no-repeat;
     min-height: 100vh;
@@ -69,6 +70,59 @@
   .head {
     margin-right: 1000px;
     margin-top: -120px;
-  }
+  } */
+  .container {
+  background-image: url('https://static.zara.net/photos///contents/mkt/spots/ss23-help-customer/subhome-xmedia-10-north//w/1839/IMAGE-landscape-web-27470945-8c31-433a-9eaf-392114b6a91d-default_0.jpg?ts=1682518360676');
+  background-repeat: no-repeat;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 10px;
+}
+
+.title {
+  font-size: 32px;
+  margin-bottom: 20px;
+  color: #fff;
+}
+
+.searchInput {
+  margin-top: 20px;
+  width: 40%;
+  outline: none;
+  border: none;
+  border-bottom: 1px solid #080808;
+  padding: 10px 0;
+  font-size: 20px;
+  height: 44px;
+  line-height: 24px;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  font-weight: bold;
+  background-color: transparent;
+  color: #fff;
+}
+
+.searchButton {
+  margin-top: 10px;
+  padding: 12px 24px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.searchButton:hover {
+  background-color: #0056b3;
+}
+
+.answer {
+  margin-top: 20px;
+  font-size: 18px;
+  color: #fff;
+}
   </style>
   
